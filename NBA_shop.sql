360abf8476281ad4039c12ece9808bcfd8d770c7
Filip Todorovic <filip.tod95@gmail.com>
2022-05-30 12:41:07 +0200

    Update NBA_shop.sql

--- NBA_shop.sql
+++ NBA_shop.sql
@@ -89,34 +89,80 @@ ALTER TABLE oprema add FOREIGN KEY(igrac) REFERENCES igrac(sifra);
         (null,'La-Clipers', 'Tyronn-Lue', '0', 'Crypto.com_Arena'),                   #6 Clipers
         (null,'Memphis-Grizzlies','Taylor-Jenkins', '0', 'FedExForum'),               #7 Memphis
         (null,'San_Antonio-Spurs', 'Gregg-Popovich', '5', 'AT&T-Center'),             #8 San Antonio
-        (null, 'Oklahoma-City-Thunder', 'Mark Daigneault', '1', 'Paycom-Center'),      #9 Oklahoma
+        (null, 'Oklahoma-City-Thunder', 'Mark Daigneault', '1', 'Paycom-Center'),     #9 Oklahoma
         (null,'New-Orleans-Pelicans','Willie Green','0', 'Smoothie-King-Center'),     #10 Pelicans
         (null,'Denver-Nugets', 'Michael-Malone', '0', 'Ball Arena'),                  #11 Denver
-        (null, 'Portlands-Trailblaizers', 'Chauncey_Billups', '1', 'Moda Center'),     #12 Portland
-        (null, 'Sacramento-Kings','Mike-Brown', '1','Golden-1-Center'),                #13 Sacramento
+        (null, 'Portlands-Trailblaizers', 'Chauncey_Billups', '1', 'Moda Center'),    #12 Portland
+        (null, 'Sacramento-Kings','Mike-Brown', '1','Golden-1-Center'),               #13 Sacramento
         (null,'Utah-Jazz','Quin Snyder', '0', 'Vivint-Arena'),                        #14 utah
-        (null, 'Minesota-Timberwolves', 'Chris-Finch', '0', 'Target-Center');          #15Minesota     # western
+        (null, 'Minesota-Timberwolves', 'Chris-Finch', '0', 'Target-Center'),         #15Minesota     # western
 
 
-    --    ('Miami-Heat', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),            #16 Miami     # EAST  
-    --    ('Boston-Celtis', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),         #17 Boston  
-    --    ('Milwakee-Bucs', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),         #18 Milwakee
-    --    ('Brookly-Nets', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),          #19 Brooklyn
-    --    ('Chikago-Bulls', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),         #20 Bulls
-    --    ('Washington-wizards', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),    #21 Washington
-    --    ('Orlando-Magic', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),         #22 Orlando
-    --    ('Atlanta-Hawks', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),         #23 Atlanta
-    --    ('Pheonix-suns', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),          #24 Pheonix
-    --    ('Toronto-Raptors', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),       #25 Toronto
-    --    ('Detroit-Pistons', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),       #26 Detroit
-    --    ('NY-knicks', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),             #27 Knicks
-    --    ('Charlotte-Hornets', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),     #28 Charlotte
-    --    ('Pheonix-suns', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),          #29 Pheonix
-    --    ('Indiana-Pacers', 'Monty-Williams', '0', '2021-10-19', '2022-04-10'),        #30 indiana
-    --    ('Philladelphia-76ers', 'Monty-Williams', '0', '2021-10-19', '2022-04-10');   #31 Philadelphia 76    #EAST
+       (null,'Miami-Heat', 'Erik-Spoelstra', '3', 'FTX-Arena'),                      #16 Miami     # EAST  
+       (null,'Boston-Celtis', 'Ime-Udoka', '17', 'TD-Garden'),                        #17 Boston  
+       (null,'Milwakee-Bucs', 'Mike-Budenholzer', '2', 'Fiserv Forum'),              #18 Milwakee
+       (null,'Brookly-Nets', 'Steve-Nash', '0', 'Barclays-Center'),                  #19 Brooklyn
+       (null,'Chikago-Bulls', 'Billy-Donovan', '6', 'United-CEnter'),                #20 Bulls
+       (null,'Washington-wizards', 'Wes-Unseld-Jr.', '1', 'Capital-One'),            #21 Washington
+       (null,'Orlando-Magic', 'Jamahl-Mosley', '0', 'Amway-Center'),                  #22 Orlando
+       (NULL,'Atlanta-Hawks', 'Nate-McMillan', '1', 'State-Farm-Arena'),             #23 Atlanta
+       (null,'Toronto-Raptors', 'Nick-Nurse', '1', 'Scotiabank-Arena'),              #24 Toronto
+       (null,'Detroit-Pistons', 'Dwane-Casey', '3', 'Little-Caesars-Arena'),          #25 Detroit
+       (null,'NY-knicks', 'Tom-Thibodeau', '2', 'Madison-Square-Garden'),             #26 Knicks
+       (null,'Charlotte-Hornets', 'James-Borrego', '0','Spectrum-Center' ),          #27 Charlotte
+       (null,'Cleveland-Cavaliers', 'Kevin-Stefanski', '1','Rocket-Mortgage-FieldHouse' ),  #28 Cleveland
+       (null,'Indiana-Pacers', 'Rick-Carlisle', '0', 'Gainbridge-Fieldhouse'),       #29 indiana
+       (null,'Philladelphia-76ers', 'Doc-Rivers', '3', 'Wells-Fargo-Center');        #30 Philadelphia 76    #EAST
 
+# IGRAČ
 
+insert into igrac ( sifra, ime, prezime, rings_count, nba_team)
+values            (null,'LeBron', 'James', '4', 1 ),
+                  (null,'Russel', 'Westbrook', '0', 1),
+                  (null, 'Anthony', 'Davis', '1', 1),         #Lakers   1
+                  (null, 'Chris', 'Paul', '0', 2),
+                  (null, 'Devin', 'Booker', '0', 2),
+                  (null, 'Jae', 'Crowder', '0', 2),          #Pheonix  2
+                  (null, 'Steph', 'Curry', '3', 3),
+                  (null, 'Klay', 'Thompson', '3', 3 ),
+                  (null, 'Draymond', 'Green', '3', 3 ),      # Golden 3
+                  (null, 'Luka', 'Dončić', '0', 4),
+                  (null, 'Tim', 'Hardaway-JR.', '0', 4),
+                  (null,'Trey', 'Burke', '0', 4),            #Dalas 4
+                  (null, 'Jhon', 'Wall', '0', 5),
+                  (null, 'Jalen', 'Green', '0', 5),
+                  (null, 'Kevin', 'Porter-JR.', '0', 5),    #Huston 5
+                  (null, 'Khawi', 'Leonard', '2', 6),
+                  (null, 'Paul', 'George', '0', 6),
+                  (null, 'Marcus', 'Morris', '0', 6),   # Clipers 6   
+                  (null, 'Ja', 'Morant', '0', 7),
+                  (null, 'Steven', 'Adams', '0', 7),
+                  (null, 'Dillon', 'Brooks', '0', 7),     #Memphis 7      
+                  (null, 'Dejounte', 'Murray', '0', 8),
+                  (null, 'Loonie', 'Walker', '0', 8),
+                  (null, 'Jakob', 'Poeltl', '0', 8),    # SAN Antonio 8
+                  (null, 'Josh', 'Giddey', '0', 9),
+                  (null, 'Shai', 'Gilgeus-Alexander', '0', 9),
+                  (null, 'Tre', 'Mann', '0', 9),        #Oklahoma 9
+                  (null, 'Brandon', 'ingram', '0', 10),
+                  (null, 'CJ', 'McCollum', '0', 10), 
+                  (null, 'Zion', 'Williamson', '0', 10), #Pelicans 10
+                  (null, 'Nikola', 'Jokić', '0', 11),
+                  (null, 'Jamal', 'Murray', '0', 11),
+                  (null, 'Aaron', 'Gordon', '0', 11),  #Denver 11
+                  (null, 'Damian', 'Lillard', '0', 12),
+                  (null, 'Anfernee', 'Simons', '0', 12),
+                  (null, 'Jusuf', 'Nurkić', '0', 12),    #Portland 12
+                  (null, "De'Aron", 'Fox', '0',13),
+                  (null, 'Domantas', 'Sabonis', '0',13),
+                  (null, 'Donte', 'DiVincenzo', '1', 13),  #Kings 13
+                  (null, 'Bojan', 'Bogdanović', '0', 14),
+                  (null, 'Rudy', 'Gobert', '0', 14),
+                  (null, 'Donovan', 'Mitchell', '0', 14),    #UTAH 14
+                  (null, 'Karl-Anthony', 'Towns', '0', 15),
+                  (null, 'Anthony', 'Edwards', '0',15),
+                  (null, "D'Angelo", 'Russel', '0', 15);
 
-
+ 
 
 
